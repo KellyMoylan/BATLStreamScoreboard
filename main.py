2190fe191f10bba058f3bb7e8d9b8e0349cd2158
Kelly Moylan <kelly.moylan@gmail.com>
2019-05-01 12:06:29 -0400

    IO optimizations

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ from tkinter import filedialog
 from tkinter import *
 import os
 import sys
+
 # sys.setdefaultencoding('utf-8')
 w = 1000
 h = 600
+
 bgcolour = "white"
+
 upArrow = u"\u25B2"
 downArrow = u"\u25BC"
-checkmark = u"\u2713"
+
 throwList = ['-', '5', '3', 'D', '1', '0', '7']
-bigAxeThrowList = ['-', u"\u2717", u"\u2713",]
+bigAxeThrowList = ['-', u"\u2717", u"\u2713", ]
+
 directory = "score_files"
+
 leftAxes = [0, 0, 0, 0, 0]
 rightAxes = [0, 0, 0, 0, 0]
 leftBigAxes = [0, 0, 0, 0]
@@ -37,11 +42,11 @@ thrower2BigUpButtons = []
 thrower2BigAxeBoxes = []
 thrower2BigDownButtons = []
 
-gameCount = [-1, -1]
+gameCount = [0, 0]
 
 gameScore = []
 
-score = [0,0]
+score = [0, 0]
 
 gameBox = []
 
@@ -54,16 +59,18 @@ class application(Tk):
     def __init__(self, parent):
         Tk.__init__(self, parent)
         self.parent = parent
-        # self.geometry(str(w) + "x" + str(h))
         self.resizable(width=False, height=False)
         self.configure(background=bgcolour)
-        self.initialize()
-        self.update_axes()
-        self.update_big_axes()
-        self.change_game_count(0, 1)
-        self.change_game_count(1, 1)
+        self.initialize_ui()
+        self.set_boxes()
+        self.update_text()
+        self.update_files()
 
-    def initialize(self):
+    def initialize_ui(self):
+        global directory
+
+        directory = filedialog.askdirectory()
+
         if not os.path.exists(directory):
             os.makedirs(directory)
 
@@ -75,7 +82,8 @@ class application(Tk):
 
         matchTitleEntry = Entry(matchTitleLabelFrame, width=40, font=('ariel', 10, 'bold'), relief=SUNKEN,
                                 borderwidth=3)
-        matchTitleButton = Button(matchTitleLabelFrame, text="Set", command=lambda: self.update_text())
+        matchTitleButton = Button(
+            matchTitleLabelFrame, text="Set", command=lambda: self.update_text())
 
         matchTitleEntry.grid(row=2, column=2, sticky=E, padx=(100, 0))
         matchTitleButton.grid(row=2, column=3, sticky=W, padx=(0, 100))
@@ -83,146 +91,189 @@ class application(Tk):
         self.create_thrower1()
         self.create_thrower2()
 
-        switchButton = Button(self, text="<- SWITCH ->", command=lambda: self.switch_sides())
+        switchButton = Button(self, text="<- SWITCH ->",
+                              command=lambda: self.switch_sides())
         switchButton.grid(row=3, column=2, padx=20)
-        resetButton = Button(self, text="RESET", command=lambda: self.reset_info())
+        resetButton = Button(self, text="RESET",
+                             command=lambda: self.reset_info())
         resetButton.grid(row=4, column=2)
-        updateButton = Button(self, text="UPDATE", width=60, height=2, font=('ariel', 16, 'bold'),
-                              command=lambda: self.update_files())
-        updateButton.grid(row=6, column=1, columnspan=3, pady=5)
+
         nextGameButton = Button(self, text="Next Game", width=60, height=2, font=('ariel', 16, 'bold'),
                                 command=lambda: self.next_game())
         nextGameButton.grid(row=7, column=1, columnspan=3, pady=5)
 
-    def update_axes(self):
-        for i in range(0, thrower1AxeBoxes.__len__()):
+    def set_boxes(self):
+
+        for i in range(0, 2):
+            gameBox[i].delete(0, 'end')
+            gameBox[i].insert(0, gameCount[i])
+        for i in range(0, 2):
+            gameScore[i].delete(0, 'end')
+            gameScore[i].insert(0, score[i])
+        for i in range(0, 5):
             thrower1AxeBoxes[i].delete(0, 'end')
             thrower1AxeBoxes[i].insert(0, throwList[leftAxes[i]])
-        for i in range(0, thrower2AxeBoxes.__len__()):
             thrower2AxeBoxes[i].delete(0, 'end')
             thrower2AxeBoxes[i].insert(0, throwList[rightAxes[i]])
-        self.update_game_score()
-        self.update_files()
-
-    def update_game_score(self):
-        score[0] = sum([int(x) for x in [throwList[y] for y in leftAxes] if str(x).isdigit()])
-        score[1] = sum([int(x) for x in [throwList[y] for y in rightAxes] if str(x).isdigit()])
-        gameScore[0].delete(0, 'end')
-        gameScore[0].insert(0, score[0])
-        gameScore[1].delete(0, 'end')
-        gameScore[1].insert(0, score[1])
-
-    def update_big_axes(self):
-        for i in range(0, thrower1BigAxeBoxes.__len__()):
+        for i in range(0, 3):
             thrower1BigAxeBoxes[i].delete(0, 'end')
-            if (i != 3):
-                thrower1BigAxeBoxes[i].insert(0, bigAxeThrowList[leftBigAxes[i]])
-            else:
-                thrower1BigAxeBoxes[i].insert(0, throwList[leftBigAxes[i]])
-        for i in range(0, thrower2BigAxeBoxes.__len__()):
+            thrower1BigAxeBoxes[i].insert(0, bigAxeThrowList[leftBigAxes[i]])
             thrower2BigAxeBoxes[i].delete(0, 'end')
-            if (i != 3):
-                thrower2BigAxeBoxes[i].insert(0, bigAxeThrowList[rightBigAxes[i]])
-            else:
-                thrower2BigAxeBoxes[i].insert(0, throwList[rightBigAxes[i]])
-        self.update_files()
+            thrower2BigAxeBoxes[i].insert(0, bigAxeThrowList[rightBigAxes[i]])
+        thrower1BigAxeBoxes[3].delete(0, 'end')
+        thrower1BigAxeBoxes[3].insert(0, throwList[leftBigAxes[3]])
+        thrower2BigAxeBoxes[3].delete(0, 'end')
+        thrower2BigAxeBoxes[3].insert(0, throwList[rightBigAxes[3]])
+
+    def update_game_score(self, side):
+        if side == 0:
+            score[0] = sum([int(x) for x in [throwList[y]
+                                         for y in leftAxes] if str(x).isdigit()])
+            gameScore[0].delete(0, 'end')
+            gameScore[0].insert(0, score[0])
+            file = open(directory + "/thrower_1_game_score.txt", "w")
+            file.write(str(score[0]))
+            file.close()
+        else:
+            score[1] = sum([int(x) for x in [throwList[y]
+                                         for y in rightAxes] if str(x).isdigit()])
+            gameScore[1].delete(0, 'end')
+            gameScore[1].insert(0, score[1])
+            file = open(directory + "/thrower_2_game_score.txt", "w")
+            file.write(str(score[1]))
+            file.close()
 
     def change_axe(self, position, direction, side):
-        if (position != 4):
-            if (side == 0):
+        global directory
+
+        if position != 4:
+            if side == 0:
                 if ((direction == 1 and leftAxes[position] < len(throwList) - 2) or (
                         direction == -1 and leftAxes[position] > 0)):
                     leftAxes[position] += direction
-                    self.update_axes()
+                    thrower1AxeBoxes[position].delete(0, 'end')
+                    thrower1AxeBoxes[position].insert(0, throwList[leftAxes[position]])
+                    file = open(f"{directory}/thrower_1_axe_{position + 1}.txt", "w")
+                    file.write(str(throwList[leftAxes[position]]))
+                    file.close()
+                    self.update_game_score(side)
             else:
                 if ((direction == 1 and rightAxes[position] < len(throwList) - 2) or (
                         direction == -1 and rightAxes[position] > 0)):
                     rightAxes[position] += direction
-                    self.update_axes()
+                    thrower2AxeBoxes[position].delete(0, 'end')
+                    thrower2AxeBoxes[position].insert(0, throwList[rightAxes[position]])
+                    file = open(f"{directory}/thrower_2_axe_{position + 1}.txt", "w")
+                    file.write(str(throwList[rightAxes[position]]))
+                    file.close()
+                    self.update_game_score(side)
         else:
-            if (side == 0):
+            if side == 0:
                 if ((direction == 1 and leftAxes[position] < len(throwList) - 1) or (
                         direction == -1 and leftAxes[position] > 0)):
                     leftAxes[position] += direction
-                    self.update_axes()
+                    thrower1AxeBoxes[position].delete(0, 'end')
+                    thrower1AxeBoxes[position].insert(0, throwList[leftAxes[position]])
+                    file = open(f"{directory}/thrower_1_axe_{position + 1}.txt", "w")
+                    file.write(str(throwList[leftAxes[position]]))
+                    file.close()
+                    self.update_game_score(side)
             else:
                 if ((direction == 1 and rightAxes[position] < len(throwList) - 1) or (
                         direction == -1 and rightAxes[position] > 0)):
                     rightAxes[position] += direction
-                    self.update_axes()
+                    thrower2AxeBoxes[position].delete(0, 'end')
+                    thrower2AxeBoxes[position].insert(0, throwList[rightAxes[position]])
+                    file = open(f"{directory}/thrower_2_axe_{position + 1}.txt", "w")
+                    file.write(str(throwList[rightAxes[position]]))
+                    file.close()
+                    self.update_game_score(side)
 
     def change_big_axe(self, position, direction, side):
-        if (position != 3):
-            if (side == 0):
+        global directory
+
+        if position != 3:
+            if side == 0:
                 if ((direction == 1 and leftBigAxes[position] < len(bigAxeThrowList) - 1) or (
                         direction == -1 and leftBigAxes[position] > 0)):
                     leftBigAxes[position] += direction
-                    self.update_big_axes()
+                    thrower1BigAxeBoxes[position].delete(0, 'end')
+                    thrower1BigAxeBoxes[position].insert(0, bigAxeThrowList[leftBigAxes[position]])
+                    file = open(f"{directory}/thrower_1_big_axe_{position + 1}.txt", "wb")
+                    file.write(bigAxeThrowList[leftBigAxes[position]].encode('utf8'))
+                    file.close()
             else:
                 if ((direction == 1 and rightBigAxes[position] < len(bigAxeThrowList) - 1) or (
                         direction == -1 and rightBigAxes[position] > 0)):
                     rightBigAxes[position] += direction
-                    self.update_big_axes()
+                    thrower2BigAxeBoxes[position].delete(0, 'end')
+                    thrower2BigAxeBoxes[position].insert(0, bigAxeThrowList[rightBigAxes[position]])
+                    file = open(f"{directory}/thrower_2_big_axe_{position + 1}.txt", "wb")
+                    file.write(bigAxeThrowList[rightBigAxes[position]].encode('utf8'))
+                    file.close()
         else:
-            if (side == 0):
+            if side == 0:
                 if ((direction == 1 and leftBigAxes[position] < len(throwList) - 1) or (
                         direction == -1 and leftBigAxes[position] > 0)):
                     leftBigAxes[position] += direction
-                    self.update_big_axes()
+                    thrower1BigAxeBoxes[position].delete(0, 'end')
+                    thrower1BigAxeBoxes[position].insert(0, throwList[leftBigAxes[position]])
+                    file = open(f"{directory}/thrower_1_big_axe_point.txt", "w")
+                    file.write(str(throwList[leftBigAxes[position]]))
+                    file.close()
             else:
                 if ((direction == 1 and rightBigAxes[position] < len(throwList) - 1) or (
                         direction == -1 and rightBigAxes[position] > 0)):
                     rightBigAxes[position] += direction
-                    self.update_big_axes()
+                    thrower2BigAxeBoxes[position].delete(0, 'end')
+                    thrower2BigAxeBoxes[position].insert(0, throwList[rightBigAxes[position]])
+                    file = open(f"{directory}/thrower_2_big_axe_point.txt", "w")
+                    file.write(str(throwList[rightBigAxes[position]]))
+                    file.close()
 
     def change_game_count(self, side, direction):
-        if (gameCount[side] != 0 or direction == 1):
+        if gameCount[side] != 0 or direction == 1:
             gameCount[side] += direction
             gameBox[side].delete(0, 'end')
             gameBox[side].insert(0, gameCount[side])
-            self.update_files()
-        elif (direction == 0):
-            gameBox[0].delete(0, 'end')
-            gameBox[0].insert(0, gameCount[0])
-            gameBox[1].delete(0, 'end')
-            gameBox[1].insert(0, gameCount[1])
-            self.update_files()
 
     def set_folder(self):
         global folderPath
         folderPath = filedialog.askdirectory()
         pathSetEntry.delete(0, 'end')
         pathSetEntry.insert(0, folderPath)
-        self.update_files()
 
     def switch_sides(self):
         self.switch_info()
-        self.update_files()
 
     def next_game(self):
-        self.determine_winner()
-        self.switch_info()
-        self.reset_axes()
-        self.update_files()
+        winner_determined = self.determine_winner()
+        if winner_determined:
+            self.reset_axes()
+            self.switch_info()
 
     def update_game_count(self):
         gameBox[0].delete(0, 'end')
         gameBox[0].insert(0, gameCount[0])
         gameBox[1].delete(0, 'end')
         gameBox[1].insert(0, gameCount[1])
-        self.update_files()
 
     def determine_winner(self):
-        leftSum = sum([int(x) for x in [throwList[y] for y in leftAxes] if str(x).isdigit()]) + sum(leftBigAxes)
-        rightSum = sum([int(x) for x in [throwList[y] for y in rightAxes] if str(x).isdigit()]) + sum(rightBigAxes)
+        leftSum = sum([int(x) for x in [throwList[y]
+                                        for y in leftAxes] if str(x).isdigit()]) + sum(leftBigAxes)
+        rightSum = sum([int(x) for x in [throwList[y]
+                                         for y in rightAxes] if str(x).isdigit()]) + sum(rightBigAxes)
         if leftSum > rightSum:
             self.change_game_count(0, 1)
-        else:
+            return True
+        elif rightSum > leftSum:
             self.change_game_count(1, 1)
+            return True
+        return False
 
     def switch_info(self):
         gameCount[0], gameCount[1] = gameCount[1], gameCount[0]
-        self.update_game_count()
+        score[0], score[1] = score[1], score[0]
         temp = throwerNames[0].get()
         throwerNames[0].delete(0, 'end')
         throwerNames[0].insert(0, throwerNames[1].get())
@@ -231,19 +282,22 @@ class application(Tk):
         global leftAxes
         global rightAxes
         leftAxes, rightAxes = rightAxes, leftAxes
-        self.update_axes()
+        self.set_boxes()
         self.update_text()
+        self.update_files()
 
     def reset_info(self):
         self.reset_axes()
         gameCount[0] = 0
         gameCount[1] = 0
-        self.change_game_count(0, 0)
-        self.change_game_count(1, 0)
+        score[0] = 0
+        score[1] = 0
         throwerNames[0].delete(0, 'end')
         throwerNames[1].delete(0, 'end')
         global matchTitleEntry
         matchTitleEntry.delete(0, 'end')
+        self.set_boxes()
+        self.update_text()
         self.update_files()
 
     def reset_axes(self):
@@ -251,12 +305,12 @@ class application(Tk):
         global rightAxes
         global leftBigAxes
         global rightBigAxes
+        score[0] = 0
+        score[1] = 0
         leftAxes = [0, 0, 0, 0, 0]
         rightAxes = [0, 0, 0, 0, 0]
         leftBigAxes = [0, 0, 0, 0]
         rightBigAxes = [0, 0, 0, 0]
-        self.update_axes()
-        self.update_big_axes()
 
     def update_text(self):
         # Match Name
@@ -366,10 +420,12 @@ class application(Tk):
         throwerNames.append(Entry(thrower1Frame, width=40))
         throwerNames[0].grid(row=1, column=3)
 
-        thrower1NameButton = Button(thrower1Frame, text="SET", command=lambda: self.update_text())
+        thrower1NameButton = Button(
+            thrower1Frame, text="SET", command=lambda: self.update_text())
         thrower1NameButton.grid(row=1, column=4, padx=10)
 
-        thrower1GameLabel = Label(thrower1Frame, font=('ariel', 10, 'bold'), bg=bgcolour, text="Game Count")
+        thrower1GameLabel = Label(thrower1Frame, font=(
+            'ariel', 10, 'bold'), bg=bgcolour, text="Game Count")
         thrower1GameLabel.grid(row=2, column=3)
 
         gameBox.append(Entry(thrower1Frame, width=5, font=('ariel', 30, 'bold'), justify=CENTER, relief=SUNKEN,
@@ -385,7 +441,7 @@ class application(Tk):
         thrower1GameCountMinus.grid(row=3, column=3, sticky=W)
 
         gameScore.append(Entry(thrower1Frame, width=5, font=('ariel', 30, 'bold'), justify=CENTER, relief=SUNKEN,
-                             borderwidth=3))
+                               borderwidth=3))
         gameScore[0].grid(row=4, column=3)
 
         thrower1AxeFrame = LabelFrame(thrower1Frame, bg=bgcolour, width=thrower1Frame.winfo_reqwidth() - 20, height=100,
@@ -467,10 +523,12 @@ class application(Tk):
         throwerNames.append(Entry(thrower2Frame, width=40))
         throwerNames[1].grid(row=1, column=3)
 
-        thrower2NameButton = Button(thrower2Frame, text="SET", command=lambda: self.update_text())
+        thrower2NameButton = Button(
+            thrower2Frame, text="SET", command=lambda: self.update_text())
         thrower2NameButton.grid(row=1, column=4, padx=10)
 
-        thrower2GameLabel = Label(thrower2Frame, font=('ariel', 10, 'bold'), bg=bgcolour, text="Game Count")
+        thrower2GameLabel = Label(thrower2Frame, font=(
+            'ariel', 10, 'bold'), bg=bgcolour, text="Game Count")
         thrower2GameLabel.grid(row=2, column=3)
 
         gameBox.append(
